6777d68d6e323da1f2fb52618205781a0e21d80a
FerDevelopment <Fernandoaaronmb@gmail.com>
2025-01-21 19:57:29 +0100

    Falta agregar pantalla informacion, Alert de reproducir. Tambien falta implementar la base de datos interna. Falta tambien agregar el Rating bar. Falta tambien agregar la pantalla con las pelis vistas y la lista de la misma

--- app/src/main/java/com/example/netflixfeo/ui/PeliculasApp.kt
+++ app/src/main/java/com/example/netflixfeo/ui/PeliculasApp.kt
@@ -1,14 +1,19 @@
 package com.example.netflixfeo.ui
 
+import android.graphics.Color.rgb
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +22,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -30,116 +36,138 @@ import com.example.netflixfeo.ui.viewModel.PeliculaViewModel
 import com.example.netflixfeo.ui.viewModel.PeliculasUIState
 
 enum class Pantallas(
-        @StringRes
-        val titulo : Int
-                    ) {
-    PantallaPelis(titulo = R.string.pelis) ,
+   @StringRes
+   val titulo: Int
+) {
+   PantallaPelis(titulo = R.string.pelis),
 
 }
 
 val listaRutas = listOf(
+   Ruta(
+      Pantallas.PantallaPelis.titulo,
+      Pantallas.PantallaPelis.name,
+      Icons.Filled.PlayArrow,
+      Icons.Outlined.PlayArrow
+   )
+   /* Ruta(
+        Pantallas.PantallaFlores.titulo ,
+        Pantallas.PantallaFlores.name ,
+        Icons.Filled.ShoppingCart ,
+        Icons.Outlined.ShoppingCart
+        ) ,
     Ruta(
-        Pantallas.PantallaPelis.titulo ,
-        Pantallas.PantallaPelis.name ,
-        Icons.Filled.PlayArrow ,
-        Icons.Outlined.PlayArrow
-        )
-    /* Ruta(
-         Pantallas.PantallaFlores.titulo ,
-         Pantallas.PantallaFlores.name ,
-         Icons.Filled.ShoppingCart ,
-         Icons.Outlined.ShoppingCart
-         ) ,
-     Ruta(
-         Pantallas.PantallaClientes.titulo ,
-         Pantallas.PantallaClientes.name ,
-         Icons.Filled.AccountCircle ,
-         Icons.Outlined.AccountCircle
-         ) ,
-     Ruta(
-         Pantallas.PantallaEmpleado.titulo ,
-         Pantallas.PantallaEmpleado.name ,
-         Icons.Filled.Face ,
-         Icons.Outlined.Face
-         ) ,
-     Ruta(
-         Pantallas.PantallaFloresMarchitadas.titulo ,
-         Pantallas.PantallaFloresMarchitadas.name ,
-         Icons.Filled.Share ,
-         Icons.Outlined.Share
-         ) ,
-     Ruta(
-         Pantallas.PantallaTipoFlores.titulo ,
-         Pantallas.PantallaTipoFlores.name ,
-         Icons.Filled.Menu ,
-         Icons.Outlined.Menu
-         )*/
-                       )
+        Pantallas.PantallaClientes.titulo ,
+        Pantallas.PantallaClientes.name ,
+        Icons.Filled.AccountCircle ,
+        Icons.Outlined.AccountCircle
+        ) ,
+    Ruta(
+        Pantallas.PantallaEmpleado.titulo ,
+        Pantallas.PantallaEmpleado.name ,
+        Icons.Filled.Face ,
+        Icons.Outlined.Face
+        ) ,
+    Ruta(
+        Pantallas.PantallaFloresMarchitadas.titulo ,
+        Pantallas.PantallaFloresMarchitadas.name ,
+        Icons.Filled.Share ,
+        Icons.Outlined.Share
+        ) ,
+    Ruta(
+        Pantallas.PantallaTipoFlores.titulo ,
+        Pantallas.PantallaTipoFlores.name ,
+        Icons.Filled.Menu ,
+        Icons.Outlined.Menu
+        )*/
+)
 
 @Composable
 fun PeliculasApp(
-        viewModel : PeliculaViewModel = viewModel(factory = PeliculaViewModel.Factory) ,
-        navController : NavHostController = rememberNavController() ,
-                ) {
-    var selectedItem by remember { mutableIntStateOf(0) }
+   viewModel: PeliculaViewModel = viewModel(factory = PeliculaViewModel.Factory),
+   navController: NavHostController = rememberNavController(),
+) {
+   var selectedItem by remember { mutableIntStateOf(0) }
 
-    Scaffold(
-        bottomBar = {
-            NavigationBar {
-                listaRutas.forEachIndexed { indice , ruta ->
-                    NavigationBarItem(
-                        icon = {
-                            if (selectedItem == indice)
-                                Icon(
-                                    imageVector = ruta.iconoLleno ,
-                                    contentDescription = stringResource(id = ruta.nombre)
-                                    )
-                            else
-                                Icon(
-                                    imageVector = ruta.iconoVacio ,
-                                    contentDescription = stringResource(id = ruta.nombre)
-                                    )
-                        } ,
-                        label = { Text(stringResource(id = ruta.nombre)) } ,
-                        selected = selectedItem == indice ,
-                        onClick = {
-                            selectedItem = indice
-                            navController.navigate(ruta.ruta)
+   Scaffold(
+      bottomBar = {
+         NavigationBar {
+            listaRutas.forEachIndexed { indice, ruta ->
+               NavigationBarItem(
+                  icon = {
+                     if (selectedItem == indice)
+                        FilledIconButton(
+                           onClick = {},
+                           colors = IconButtonColors(
+                              contentColor = Color(rgb(160, 71, 71)),
+                              disabledContentColor = Color.Yellow,
+                              disabledContainerColor = Color.Red,
+                              containerColor = Color(rgb(238, 223, 122))
+                           )
+                        ) {
+                           Icon(
+                              imageVector = ruta.iconoLleno,
+                              contentDescription = stringResource(id = ruta.nombre),
+                           )
                         }
-                                     )
-                }
+                     else
+                        OutlinedIconButton(
+                           onClick = {},
+                           colors = IconButtonColors(
+                              contentColor = Color(rgb(160, 71, 71)),
+                              disabledContentColor = Color.Yellow,
+                              disabledContainerColor = Color.Red,
+                              containerColor = Color(rgb(238, 223, 122))
+                           )
+                        ) {
+                           Icon(
+                              imageVector = ruta.iconoVacio,
+                              contentDescription = stringResource(id = ruta.nombre),
+                           )
+                        }
+                  },
+                  label = { Text(stringResource(id = ruta.nombre)) },
+                  selected = selectedItem == indice,
+                  onClick = {
+                     selectedItem = indice
+                     navController.navigate(ruta.ruta)
+                  },
+                  modifier = Modifier.background(Color.Transparent)
+               )
             }
-        } ,
-        modifier = Modifier.fillMaxSize()
-            ) { innerPadding ->
-        val uiState = viewModel.peliculasUIState
-        NavHost(
-            navController = navController ,
-            startDestination = Pantallas.PantallaPelis.name ,
-            modifier = Modifier.padding(innerPadding)
-               ) {
-            // Grafo de las rutas
-            composable(route = Pantallas.PantallaPelis.name) {
-                /*Pantalla inicial*/
+         }
+      },
+      modifier = Modifier.fillMaxSize()
+   ) { innerPadding ->
+      val uiState = viewModel.peliculasUIState
+      NavHost(
+         navController = navController,
+         startDestination = Pantallas.PantallaPelis.name,
+         modifier = Modifier.padding(innerPadding)
+      ) {
+         // Grafo de las rutas
+         composable(route = Pantallas.PantallaPelis.name) {
+            /*Pantalla inicial*/
 
-                when (uiState) {
-                    is PeliculasUIState.ObtenerPelis -> {
-                        MostrarPelis(
-                            uiState.pelis ,
-                            onClickPeli = {
-                                viewModel
-                                        .actualizarPeliculaPulsada(it)
-                            } ,
-                            viewModel.peliculaSelcionada
+            when (uiState) {
+               is PeliculasUIState.ObtenerPelis -> {
+                  MostrarPelis(
+                     uiState.pelis,
+                     onClickPeli = {
+                        viewModel
+                           .actualizarPeliculaPulsada(it)
+                     },
+                     viewModel.peliculaSelcionada,
+                     onClickReproducir = {},
+                     onClickEstadistica = {}
+                  )
+               }
 
-                            )
-                    }
-
-                    else -> {
-                        viewModel.obtenerPelis()
-                    }
-                }
+               else -> {
+                  viewModel.obtenerPelis()
+               }
             }
-        }
-    }
+         }
+      }
+   }
 }
--- app/src/main/java/com/example/netflixfeo/ui/pantallas/PantallaPelis.kt
+++ app/src/main/java/com/example/netflixfeo/ui/pantallas/PantallaPelis.kt
@@ -1,39 +1,112 @@
 package com.example.netflixfeo.ui.pantallas
 
+import android.graphics.Color.rgb
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.netflixfeo.modelo.Pelicula
 import com.example.netflixfeo.ui.componentes.CaratulaPeli
 
 @Composable
-fun MostrarPelis(
-        peliculas : List<Pelicula> ,
-        onClickPeli : (Pelicula) -> Unit ,
-        peliculaSelcionada : Pelicula
-                ) {
-    Column(modifier = Modifier.padding(10.dp)) {
-        LazyRow {
-            items(peliculas) { peli ->
-                CaratulaPeli(peli , onClickPeli = onClickPeli)
-            }
-        }
-        MostrarPeli(peliculaSelcionada)
-    }
+fun MostrarPelis(peliculas: List<Pelicula>,
+                 onClickPeli: (Pelicula) -> Unit,
+                 peliculaSelcionada: Pelicula,
+                 onClickEstadistica: () -> Unit,
+                 onClickReproducir: () -> Unit
+) {
+   Column(modifier = Modifier.padding(10.dp)) {
+      LazyRow {
+         items(peliculas) { peli ->
+            CaratulaPeli(peli, onClickPeli = onClickPeli)
+         }
+      }
+      Row {
+         MostrarPeli(peliculaSelcionada)
+         Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+         ) {
+            BTNEstadisticas(onClickEstadistica)
+            BTNReproducir(onClickReproducir)
+         }
+      }
+   }
 
 }
 
 @Composable
-fun MostrarPeli(peliculaSelcionada : Pelicula) {
-    CaratulaPeli(peliculaSelcionada , 400.dp , 300.dp)
-    Text(text = "Titulo: " + peliculaSelcionada.nombre)
+private fun BTNEstadisticas(onClickEstadistica: () -> Unit) {
+   FilledIconButton(
+      onClick = onClickEstadistica,
+      shape = CircleShape,
+      modifier = Modifier
+         .fillMaxWidth()
+         .padding(10.dp),
+      colors = IconButtonColors(
+         contentColor = Color(rgb(238, 223, 122)),
+         disabledContentColor = Color.Yellow,
+         disabledContainerColor = Color.Red,
+         containerColor = Color(rgb(160, 71, 71))
+      )
+   ) {
+      Icon(
+         contentDescription = "Boton de Informacion",
+         imageVector = Icons.Filled.Info,
+      )
+   }
+}
 
-    Text(text = "Actor principal " + peliculaSelcionada.actorPrincipal)
-    Text(text = "Duracion: " + peliculaSelcionada.duracionMinutos)
-    Text(text = "Director: " + peliculaSelcionada.director)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BTNReproducir(onClickReproducir: () -> Unit) {
+   FilledIconButton(
+      onClick = onClickReproducir,
+      shape = CircleShape,
+      modifier = Modifier
+         .fillMaxWidth()
+         .padding(10.dp),
+      colors = IconButtonColors(
+         contentColor = Color(rgb(160, 71, 71)),
+         disabledContentColor = Color.Yellow,
+         disabledContainerColor = Color.Red,
+         containerColor = Color(rgb(238, 223, 122))
+      )
+   ) {
+      Icon(
+         contentDescription = "Boton de reproducir",
+         imageVector = Icons.Filled.PlayArrow,
+      )
+   }
+}
+
+@Composable
+fun MostrarPeli(peliculaSelcionada: Pelicula) {
+   Column {
+      CaratulaPeli(peliculaSelcionada, 350.dp, 250.dp)
+      Text(text = "Titulo: " + peliculaSelcionada.nombre)
+      Text(text = "Actor principal " + peliculaSelcionada.actorPrincipal)
+      Text(text = "Duracion: " + peliculaSelcionada.duracionMinutos)
+      Text(text = "Director: " + peliculaSelcionada.director)
+   }
+
 }
